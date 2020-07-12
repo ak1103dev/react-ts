da2ed2dbdf950e4cff24053e7f2f9cc05bede6ba
ak1103dev <ak.apichan1103@gmail.com>
2020-07-12 16:37:31 +0700

    :construction: login form

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 type Props = {
   title: string
+  type?: 'button' | 'submit' | 'reset'
   onClick?: any
 }
 
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,26 +1,63 @@
 import React from 'react'
-import FormInput from 'components/Form/Input'
+import { useHistory } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers'
+import * as yup from 'yup'
 import Button from 'components/Button'
+import Swal from 'sweetalert2'
+
+type FormData = {
+  email: string
+  password: string
+}
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+})
 
 const LoginPage = () => {
-  const onLogin = async () => {
+  const history = useHistory()
+  const { register, handleSubmit, errors } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  })
+
+  const onLogin = async (values: FormData) => {
     try {
-      const credentials = {
-        email: 'ak@yopmail.com',
-        password: 'password',
+      if (values.email === 'ak@yopmail.com' && values.password === '1234') {
+        Swal.fire({
+          title: 'Good job!',
+          text: 'You clicked the button!',
+          icon: 'success',
+        })
+        history.push('/')
+      } else {
+        throw new Error('Email or password is incorrect')
       }
-      console.log({ credentials })
     } catch (e) {
-      console.log(e)
+      Swal.fire({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool',
+      })
     }
   }
 
   return (
     <div>
       <h2>Login</h2>
-      <FormInput label="Email" name="email" value="example@email.com" />
-      <FormInput label="Password" name="password" value="password" />
-      <Button title="Login" onClick={onLogin} />
+      <form onSubmit={handleSubmit(onLogin)}>
+        <label>Email</label>
+        <input name="email" ref={register} />
+        <p>{errors.email?.message}</p>
+
+        <label>Password</label>
+        <input name="password" ref={register} />
+        <p>{errors.password?.message}</p>
+
+        <Button type="submit" title="Login" />
+      </form>
     </div>
   )
 }
