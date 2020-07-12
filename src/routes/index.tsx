ac15f4c64a4cdb8913bb3bd944f7ad2014c81e4a
ak1103dev <ak.apichan1103@gmail.com>
2020-07-12 17:35:44 +0700

    :construction: code spliting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { renderRoutes, RouteConfig } from 'react-router-config'
-import { BrowserRouter } from 'react-router-dom'
-
+import { BrowserRouter as Router } from 'react-router-dom'
 import Root from 'layouts/Root'
-import Home from 'pages/Home'
-import Login from 'pages/Login'
+import ErrorBoundary from 'components/ErrorBoundary'
+
+const Home = lazy(() => import('pages/Home'))
+const Login = lazy(() => import('pages/Login'))
 
 type Props = {
   route: RouteConfig
@@ -54,7 +55,15 @@ const routes: any = [
 ]
 
 const Routes = () => {
-  return <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
+  return (
+    <Router>
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading...</div>}>
+          {renderRoutes(routes)}
+        </Suspense>
+      </ErrorBoundary>
+    </Router>
+  )
 }
 
 export default Routes
